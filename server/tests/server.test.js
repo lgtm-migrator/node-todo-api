520c831e3d579d0925c7fc7c4b59c4a2d525b02b
Bobby Lin <mr.linjiahao@gmail.com>
2017-05-16 17:30:43 +0800

    Add testcases for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,8 +8,15 @@ const {ObjectID} = require('mongodb');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
-const todos = [{_id: new ObjectID(), text: "First text"},
-  {_id: new ObjectID(), text: "Second text"}]; // dummy values
+const todos = [{
+  _id: new ObjectID(),
+  text: "First text"},
+  {
+    _id: new ObjectID(), //For testing PATCH
+    text: "Second text",
+    completed: true,
+    completedAt: 18888}
+  ]; // dummy values
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -138,4 +145,46 @@ describe('DELETE /todos/:id', () => {
     .expect(404)
     .end(done);
   })
+});
+
+describe('PATCH /todos/:id', ()=> {
+  it('should update the todo', (done) => {
+    // Obtain id of first dummy document
+    var hexId = todos[0]._id.toHexString();
+    var text = "This is the correct text";
+
+    // text is changed, completed is true, completedAt is a number
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      text,
+      completed: true,
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.completed).toBe(true);
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completedAt).toBeA('number');
+    })
+    .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = "This is the updated text";
+
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      text,
+      completed: false
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toNotExist();
+    })
+    .end(done);
+  });
 });
