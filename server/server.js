a44e19ce3464cd1336873532e2169d72aa4581fc
Bobby Lin <mr.linjiahao@gmail.com>
2017-05-16 14:17:14 +0800

    Set up app for Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -53,8 +55,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Listen to port 3000');
+app.listen(port, () => {
+  console.log(`Listen to port ${port}`);
 });
 
 module.exports = {app};
