3278aebbb188bade3ecd904e5e0205697aaccb9c
Bobby Lin <mr.linjiahao@gmail.com>
2017-05-20 08:00:56 +0800

    Added test cases for GET /users/me

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,6 +96,29 @@ describe('GET /todos/:id', () => {
   })
 });
 
+describe('GET /users/me', ()=> {
+  it('should return user if authenticated', (done) => {
+    request(app)
+    .get('/users/me')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body._id).toBe(users[0]._id.toHexString());
+      expect(res.body.email).toBe(users[0].email);
+    })
+    .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+    .get('/users/me')
+    .set('x-auth', 'abcdefg')
+    .expect(401)
+    .end(done);
+  });
+
+});
+
 describe('DELETE /todos/:id', () => {
 
   it('should delete todo doc and return doc', (done) => {
